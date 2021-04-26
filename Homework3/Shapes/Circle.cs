719d03e65174b065b0f635cfd638adee1e624976
zkp55 <80044240+zkp55@users.noreply.github.com>
2021-04-26 21:02:14 +0800

    Add files via upload

---
+++ Homework3/Shapes/Circle.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework3 {
+  class Circle : Shape {
+
+    public Circle(double radius) {
+      Radius = radius;
+    }
+
+    public double Radius { get; set; }
+
+    public string Info => $"圆形:radius={Radius}";
+    
+    public double Area {
+      get {
+        if (!IsValid()) throw new InvalidOperationException("形状无效，无法计算面积");
+        return Math.PI * Radius * Radius;
+      }
+    }
+
+    public bool IsValid() {
+      return Radius > 0;
+    }
+  }
+}
---
+++ Homework3/Shapes/Main.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Homework3 {
+  class Program {
+    static void Main(string[] args) {
+      try {
+        List<Shape> shapes = new List<Shape>();
+        for (int i = 0; i < 10; i++) {
+          shapes.Add(ShapeFactory.CreateRandomShape());
+        }
+
+        shapes.ForEach(s => 
+          Console.WriteLine($"{s.Info}, area={s.Area}"));
+
+      }catch (Exception e) {
+        Console.WriteLine(e.Message);
+      }
+    }
+  }
+}

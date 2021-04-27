a8e7ac007b745b0561c7b6d404ae03dd1ecda2b7
zkp55 <80044240+zkp55@users.noreply.github.com>
2021-04-27 19:00:57 +0800

    Add files via upload

---
+++ Homework4/AlarmClock/AlarmClock.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace clock {
+
+  /// <summary>
+  /// event publisher:Clock class
+  /// </summary> 
+  class AlarmClock {
+   
+    public event Action<AlarmClock> AlarmEvent;
+
+    public event Action<AlarmClock> TickEvent;
+
+    public AlarmClock() {
+      CurrentTime = new ClockTime();
+    }
+
+    public ClockTime CurrentTime { get; set; }
+
+    public ClockTime AlarmTime { get; set; }
+
+    /// start a clock, and keep it run
+    public void Run() {
+          while (true) {
+            DateTime now = DateTime.Now;
+            CurrentTime = new ClockTime(now.Hour, now.Minute, now.Second);
+            TickEvent(this);
+            if (AlarmTime.Equals(CurrentTime))
+              AlarmEvent(this);
+            Thread.Sleep(1000);
+          }
+        }
+  }
+}
---
+++ Homework4/AlarmClock/ClockTime.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace clock {
+
+  class ClockTime {
+
+    private int hour;
+    private int minute;
+    private int second;
+
+    public ClockTime(int hour = 0, int minute = 0, int second = 0) {
+      Hour = hour;
+      Minute = minute;
+      Second = second;
+    }
+
+    public int Hour {
+      get { return hour; }
+      set {
+        if (value < 0 || value > 24) throw new ArgumentOutOfRangeException("invalid hour!");
+        hour = value;
+      }
+    }
+
+    public int Minute {
+      get { return minute; }
+      set {
+        if (value < 0 || value > 60) {
+          throw new ArgumentOutOfRangeException("minute invalid!");
+        }
+        minute = value;
+      }
+    }
+
+    public int Second {
+      get { return second; }
+      set {
+        if (value < 0 || value > 60) {
+          throw new ArgumentOutOfRangeException("invalid second!");
+        }
+        second = value;
+      }
+    }
+
+    public override bool Equals(object obj) {
+      var time = obj as ClockTime;
+      return time != null &&
+             Hour == time.Hour &&
+             Minute == time.Minute &&
+             Second == time.Second;
+    }
+
+    public override int GetHashCode() {
+      var hashCode = 1505761165;
+      hashCode = hashCode * -1521134295 + Hour.GetHashCode();
+      hashCode = hashCode * -1521134295 + Minute.GetHashCode();
+      hashCode = hashCode * -1521134295 + Second.GetHashCode();
+      return hashCode;
+    }
+  }
+}

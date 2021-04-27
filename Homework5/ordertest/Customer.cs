3a5cd87957d7d66cddd4efafc9386b24a3fc92a1
zkp55 <80044240+zkp55@users.noreply.github.com>
2021-04-27 20:47:18 +0800

    Add files via upload

---
+++ Homework5/ordertest/Customer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ordertest {
+
+  public class Customer {
+
+    public uint Id { get; set; }
+
+    public string Name { get; set; }
+
+    public Customer(uint id, string name) {
+      this.Id = id;
+      this.Name = name;
+    }
+    
+    public override string ToString() {
+      return $"customerId:{Id}, CustomerName:{Name}";
+    }
+
+    public override bool Equals(object obj) {
+      var customer = obj as Customer;
+      return customer != null &&
+             Id == customer.Id;
+    }
+
+    public override int GetHashCode() {
+      return 2108858624 + Id.GetHashCode();
+    }
+  }
+}
---
+++ Homework5/ordertest/Goods.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ordertest {
+
+ public class Goods {
+    
+    public int Id { get; set; }
+
+    public string Name { get; set; }
+
+    private float price;
+
+    public float Price {
+      get { return price; }
+      set {
+        if (value < 0)
+          throw new ArgumentOutOfRangeException("the price must be >= 0!");
+        price = value;
+      }
+    }
+   
+    public Goods(int id, string name, float price) {
+      this.Id = id;
+      this.Name = name;
+      this.Price = price;
+    }
+
+    public override bool Equals(object obj) {
+      var goods = obj as Goods;
+      return goods != null &&
+             Id == goods.Id;
+    }
+
+    public override int GetHashCode() {
+      return 2108858624 + Id.GetHashCode();
+    }
+
+    public override string ToString() {
+      return $"Id:{Id}, Name:{Name}, Value:{Price}";
+    }
+  }
+}
